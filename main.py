4b77262db98eb3c7be9ae94c6265612c8f72719c
Ardgellan <aegiev01@gmail.com>
2025-01-22 21:40:32 +0300

    start_18

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
 from aiogram import executor
 
-# import telebot
-# import pyotp
-# import time
+async def on_startup(dp):
+    import time
 
-# from source.data import config
+    from loguru import logger
+    from source import handlers
 
-# # Замените на ваш токен и секретный ключ
-# API_TOKEN = config.bot_token
-# TOTP_SECRET = config.totp_secret
+    logger.add(
+        f'logs/{time.strftime("%Y-%m-%d__%H-%M")}.log',
+        level="DEBUG",
+        rotation="500 MB",
+        compression="zip",
+    )
 
-# bot = telebot.TeleBot(API_TOKEN)
+    handlers.setup(dp)
 
-# # Хранение разрешённых пользователей (ID Telegram)
-# AUTHORIZED_USERS = config.admins_ids
+    logger.success("[+] Bot started successfully")
 
-# # Генерация TOTP кода
-# def generate_totp():
-#     totp = pyotp.TOTP(TOTP_SECRET)
-#     return totp.now()
-
-# # Команда /start
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     if message.chat.id in AUTHORIZED_USERS:
-#         bot.reply_to(message, "Привет! Отправь /code, чтобы получить TOTP код.")
-#     else:
-#         bot.reply_to(message, "Извините, у вас нет доступа к этому боту.")
-
-# # Команда /code
-# @bot.message_handler(commands=['code'])
-# def send_code(message):
-#     if message.chat.id in AUTHORIZED_USERS:
-#         code = generate_totp()
-#         bot.reply_to(message, f"Ваш код: {code}")
-#     else:
-#         bot.reply_to(message, "У вас нет доступа к этой команде.")
-
-# # Запуск бота
-# print("Бот запущен...")
-# bot.polling()
 
 if __name__ == "__main__":
     # Launch

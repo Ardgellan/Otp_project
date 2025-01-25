d929418e7e047746bf7650d8a86a5820b7f10b10
Ardgellan <aegiev01@gmail.com>
2025-01-25 20:08:11 +0300

    start_29

--- source/handlers/user/start.py
+++ source/handlers/user/start.py
@@ -2,12 +2,16 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from source.utils import localizer
+from source.keyboard import inline
 
 async def start(message: types.Message):
     await message.answer(
         text=localizer.get_user_localized_text(
             user_language_code=message.from_user.language_code,
             text_localization=localizer.message.greetings_message,
+        ),
+        reply_markup=await inline.start_menu_kb(
+            language_code=message.from_user.language_code,
         )
     )
 
@@ -16,5 +20,8 @@ async def main_menu_by_button(message: types.Message):
         text=localizer.get_user_localized_text(
             user_language_code=message.from_user.language_code,
             text_localization=localizer.message.greetings_message,
+        ),
+        reply_markup=await inline.start_menu_kb(
+            language_code=message.from_user.language_code,
         )
     )

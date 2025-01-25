9bcd98a4d48e336a5f40736cf214edd68d9b1f76
Ardgellan <aegiev01@gmail.com>
2025-01-25 19:36:53 +0300

    start_24

--- source/handlers/user/start.py
+++ source/handlers/user/start.py
@@ -5,5 +5,8 @@ from source.utils import localizer
 
 async def start(message: types.Message):
     await message.answer(
-        text=localizer.message.greetings_message
+        text=localizer.get_user_localized_text(
+            user_language_code=message.from_user.user_language_code,
+            text_localization=localizer.message.payment_confirmation_message,
+        )
     )

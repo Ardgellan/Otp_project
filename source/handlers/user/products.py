bc1b9f7da2e39035d136f4e750cbae0fcef48eab
ArdGellan <aegiev01@gmail.com>
2025-04-06 20:17:32 +0300

    start_71

--- source/handlers/user/products.py
+++ source/handlers/user/products.py
@@ -96,7 +96,7 @@ async def add_product_to_db_and_notify(message: types.Message, state: FSMContext
     # Пример добавления товара в БД (здесь должен быть твой реальный метод для добавления)
     try:
         await db_manager.add_product(
-            user_id=message.from_user.id,
+            seller_id=message.from_user.id,
             name=data['product_name'],
             product_id=data['product_id'],
             otp=data['product_otp']

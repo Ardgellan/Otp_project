9322d5170f4a76ae7fbf5239509fc26c0006a7ba
ArdGellan <aegiev01@gmail.com>
2025-05-21 20:05:06 +0300

    middle_26

--- source/database/updater.py
+++ source/database/updater.py
@@ -8,7 +8,26 @@ class Updater(DatabaseConnector):
         super().__init__()
         logger.debug("Updater object was initialized")
 
-    async def extend_user_subscription(self, seller_id: int, months: int):
+    # async def extend_user_subscription(self, seller_id: int, months: int, conn=None)-> bool:
+    #     query = f"""
+    #         UPDATE sellers
+    #         SET 
+    #             subscription_is_active = TRUE,
+    #             subscription_until = (
+    #                 CASE 
+    #                     WHEN subscription_until > NOW() THEN subscription_until + INTERVAL '{months} months'
+    #                     ELSE NOW() + INTERVAL '{months} months'
+    #                 END
+    #             ),
+    #             last_subscription_payment = NOW()
+    #         WHERE seller_id = {seller_id};
+    #     """
+    #     result = await self._execute_query(query)
+    #     logger.debug(f"Подписка пользователя {seller_id} продлена на {months} мес.")
+    #     return result
+
+
+    async def extend_user_subscription(self, seller_id: int, months: int, conn=None) -> bool:
         query = f"""
             UPDATE sellers
             SET 
@@ -22,6 +41,15 @@ class Updater(DatabaseConnector):
                 last_subscription_payment = NOW()
             WHERE seller_id = {seller_id};
         """
-        await self._execute_query(query)
-        logger.debug(f"Подписка пользователя {seller_id} продлена на {months} мес.")
-
+        try:
+            if conn:
+                await conn.execute(query)
+            else:
+                if await self._execute_query(query) is False:
+                    logger.error(f"Ошибка при продлении подписки пользователя {seller_id}")
+                    return False
+            logger.debug(f"Подписка пользователя {seller_id} продлена на {months} мес.")
+            return True
+        except Exception as e:
+            logger.error(f"Исключение при продлении подписки пользователя {seller_id}: {e}")
+            return False

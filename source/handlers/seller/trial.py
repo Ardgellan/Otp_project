941eed0d4287cb5f033bee509453e2b3eb5d1c10
ArdGellan <aegiev01@gmail.com>
2025-05-22 13:00:19 +0300

    middle_35

--- source/handlers/seller/trial.py
+++ source/handlers/seller/trial.py
@@ -11,29 +11,32 @@ async def trial_period_func(call: types.CallbackQuery, state: FSMContext):
     logger.debug("TRIAL_1")
     sub_is_active = await db_manager.is_subscription_active(call.from_user.id)
     logger.debug("TRIAL_2")
-    if not sub_is_active:
-        await call.message.answer(
-            text=localizer.get_user_localized_text(
-                user_language_code=call.from_user.language_code,
-                text_localization=localizer.message.trial_period_message
-            ),
-            parse_mode=types.ParseMode.HTML,
-            reply_markup=await inline.trial_period_keyboard(
-                user_language_code=call.from_user.language_code
+    try:
+        if not sub_is_active:
+            await call.message.answer(
+                text=localizer.get_user_localized_text(
+                    user_language_code=call.from_user.language_code,
+                    text_localization=localizer.message.trial_period_message
+                ),
+                parse_mode=types.ParseMode.HTML,
+                reply_markup=await inline.trial_period_keyboard(
+                    user_language_code=call.from_user.language_code
+                )
             )
-        )
-    else:
-        await call.message.answer(
-            text=localizer.get_user_localized_text(
-                user_language_code=call.from_user.language_code,
-                text_localization=localizer.message.trial_period_refusal_message
-            ),
-            parse_mode=types.ParseMode.HTML,
-            reply_markup=await inline.insert_button_back_to_main_menu(
-                user_language_code=call.from_user.language_code
+        else:
+            await call.message.answer(
+                text=localizer.get_user_localized_text(
+                    user_language_code=call.from_user.language_code,
+                    text_localization=localizer.message.trial_period_refusal_message
+                ),
+                parse_mode=types.ParseMode.HTML,
+                reply_markup=await inline.insert_button_back_to_main_menu(
+                    user_language_code=call.from_user.language_code
+                )
             )
-        )
-    
+    except Exception as e:
+        logger.error(f"Ошибка при отправке сообщения: {e}")
+
     await call.answer()
 
 

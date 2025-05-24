a0d77c245d0c3248bfcb5054b106dae6cf6e5364
ArdGellan <aegiev01@gmail.com>
2025-05-24 16:41:23 +0300

    middle_60

--- source/handlers/buyer/__init__.py
+++ source/handlers/buyer/__init__.py
@@ -1,13 +1,13 @@
-from aiogram import Dispatcher
-from loguru import logger
+# from aiogram import Dispatcher
+# from loguru import logger
 
-from loader import dp
+# from loader import dp
 
 
-def register_seller_handlers(dp: Dispatcher):
-    try:
+# def register_buyer_handlers(dp: Dispatcher):
+#     try:
 
-    except Exception as e:
-        logger.error(f"Error while registering seller handlers: {e}")
-    else:
-        logger.info("Seller handlers registered successfully")
+#     except Exception as e:
+#         logger.error(f"Error while registering buyer handlers: {e}")
+#     else:
+#         logger.info("Buyer handlers registered successfully")
